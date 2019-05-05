e71556c8bd801bf9ae1a380631e525196f55b8a0
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-05-05 12:29:08 +0800

    refactored using useState hook

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,17 +1,40 @@
-import React, { Component } from "react";
+// Class-based components
+// import React, { Component } from "react";
 
-class App extends Component {
-  state = { resource: "posts" };
+// class App extends Component {
+//   state = { resource: "posts" };
 
-  render() {
-    return (
-      <div>
-        <button className="btn btn-primary" onClick={() => this.setState({ resource: "posts" })}>Posts</button>
-        <button className="btn btn-primary" onClick={() => this.setState({ resource: "tasks" })}>Tasks</button>
-        <div className="resources">{this.state.resource}</div>
-      </div>
-    );
-  };
+//   render() {
+//     return (
+//       <div>
+//         <button className="btn btn-primary" onClick={() => this.setState({ resource: "posts" })}>Posts</button>
+//         <button className="btn btn-primary" onClick={() => this.setState({ resource: "tasks" })}>Tasks</button>
+//         <div className="resources">{this.state.resource}</div>
+//       </div>
+//     );
+//   };
+// };
+
+// export default App;
+
+// Function-based components
+import React, { useState } from "react";
+
+const App = () => {
+  // Refactoring to a hook:
+  // 1. resource === this.set.resource
+  // 2. setResource === this.setState({ resource })
+  // 3. useState === hook function from React
+  // 4. "posts" === initial state (state = { "..." })
+  const [resource, setResource] = useState("posts");
+
+  return (
+    <div>
+      <button className="btn btn-primary" onClick={() => setResource("posts")}>Posts</button>
+      <button className="btn btn-primary" onClick={() => setResource("tasks")}>Tasks</button>
+      <div className="resources">{resource}</div>
+    </div>
+  );
 };
 
 export default App;

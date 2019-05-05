8feb59afe11e7ed3cc75890f0e0cce106d0397f6
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-05-06 00:45:06 +0800

    refactored using useEffect hook

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,33 +1,64 @@
-import React, { Component } from "react";
+// Class-based components
+// import React, { Component } from "react";
 
-class List extends Component {
-  state = { resources: [] };
+// class List extends Component {
+//   state = { resources: [] };
 
-  fetchRequest() {
-    const url = `https://jsonplaceholder.typicode.com/${this.props.resource}`;
+//   fetchRequest() {
+//     const url = `https://jsonplaceholder.typicode.com/${this.props.resource}`;
+//     const promise = fetch(url)
+//       .then(response => response.json())
+//       .then(data => this.setState({ resources: data })
+//     );
+//   };
+
+//   componentDidMount() {
+//     this.fetchRequest();
+//   };
+
+//   componentDidUpdate(prevProps) {
+//     if (prevProps.resource !== this.props.resource) {
+//       this.fetchRequest();
+//     };
+//   };
+
+//   render() {
+//     return (
+//       <div className="resources">
+//         <ol>{this.state.resources.map((resource) => <li>{resource.title} </li>)}</ol>
+//       </div>
+//     );
+//   };
+// };
+
+// export default List;
+
+// Function-based components - using hooks
+import React, { useState, useEffect } from "react";
+
+const List = ({ resource }) => {
+  const [resources, setResource] = useState([]);
+
+  const fetchResources = () => {
+    const url = `https://jsonplaceholder.typicode.com/${resource}`;
     const promise = fetch(url)
       .then(response => response.json())
-      .then(data => this.setState({ resources: data })
+      .then(data => setResource(data)
     );
   };
 
-  componentDidMount() {
-    this.fetchRequest();
-  };
+  // Refactoring to a useEffect hook:
+  // 1. The function (i.e. fetchResources) will be rendered if the element inside [] is different in the previous state
+  // 2. useEffect essentially combines componentDidMount and componentDidUpdate
+  useEffect(() => {
+    fetchResources(resource)}, [resource]
+  );
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.resource !== this.props.resource) {
-      this.fetchRequest();
-    };
-  };
-
-  render() {
-    return (
-      <div className="resources">
-        <ol>{this.state.resources.map((resource) => <li>{resource.title} </li>)}</ol>
-      </div>
-    );
-  };
+  return (
+    <div className="resources">
+      {resource}: {resources.length}
+    </div>
+  );
 };
 
 export default List;
